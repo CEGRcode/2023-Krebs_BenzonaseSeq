b9327535fe1d1a8d2e13f5d0f54d94b895632dbf
owlang <oliviawlang@gmail.com>
2024-03-26 20:06:18 -0400

    restructure contents of 05_Figure_2/01_motif.sh
    Break up motif search and weblogo making script into 03_Call_Motifs and X_Bulk_Processing such that it runs directly scales (sbatch array)

---
+++ X_Bulk_Processing/1_make_weblogos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make weblogos for every JASPAR MEME file in `data/JASPAR/*.meme`
+
+### CHANGE ME
+WRK=/path/to/2023-Krebs_BenzonaseSeq/X_Bulk_Processing
+WRK=/storage/home/owl5022/scratch/2023-Krebs_Benzonase-seq/X_Bulk_Processing
+###
+
+# Dependencies
+# - ceqlogo
+
+set -exo
+module load anaconda
+conda activate bx
+
+# Fill in placeholder constants with your directories
+PWM=$WRK/../data/JASPAR
+OUTDIR=$WRK/Library/WebLogos
+
+# Set up output directories
+[ -d $OUTDIR ] || mkdir -p $OUTDIR
+
+# Loop through the PWM files
+for PWMFILE in $PWM/*.meme;
+do
+    BASE=`basename $PWMFILE ".meme"`
+    # Generate logo
+    ceqlogo -i $PWMFILE -m 1 -o $OUTDIR/$BASE\_logo.eps -f EPS
+done
