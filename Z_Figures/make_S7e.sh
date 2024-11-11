15193811197a131cadf11bed4486791d88d40a78
owlang <oliviawlang@gmail.com>
2024-11-11 05:38:09 -0500

    refactor X and Z for Figure S7e on hg38

--- Z_Figures/make_S7e.sh
+++ Z_Figures/make_S7e.sh
@@ -23,29 +23,29 @@ source activate /storage/group/bfp2/default/owl5022-OliviaLang/conda/bx
 BED=PlusOneDyad_SORT-Expression_2000bp
 CDIR=../X_Bulk_Processing/Library/$BED/CDT/
 FDIR=../data/BAM/NormalizationFactors
-TEMP=temp-F4d
 
 # Script shortcuts
 SCRIPTMANAGER=$WRK/../bin/ScriptManager-v0.15.jar
 VIOLIN=$WRK/../bin/make_violin_plot.py
 
-[ -d F4/d ] || mkdir -p F4/d
+[ -d S7 ] || mkdir S7
+[ -d S7/e ] || mkdir S7/e
+
+TEMP=temp-S7e
 [ -d $TEMP ] || mkdir $TEMP
 
 
 for TARGET in "H2AZ" "H2B" "H3" "H3K4me3" "H3K9ac" "H3K27ac";
 do
-	BAM=BNase-ChIP_${TARGET}_merge_hg19
+	BAM=BNase-ChIP_${TARGET}_merge_hg38
 	FACTOR=`grep '^Scaling factor' $FDIR/$BAM\_TotalTag_ScalingFactors.out | awk '{print $3}'`
 
-	# Calculate per-site Proximal
-	gzip -dc $CDIR/$BAM\_$BED\_5read1-MIN128-MAX164_sense.cdt.gz \
-		| cut -f1,2,930-999 > $TEMP/$TARGET\_Proximal_NOSCALE.cdt
-	java -jar $SCRIPTMANAGER read-analysis scale-matrix -s $FACTOR $TEMP/$TARGET\_Proximal_NOSCALE.cdt -o $TEMP/$TARGET\_Proximal.cdt
+	# Slice Proximal/Distal tag counts
+	gzip -dc $CDIR/$BAM\_$BED\_5read1-MIN128-MAX164_sense.cdt.gz | cut -f1,2,930-999 > $TEMP/$TARGET\_Proximal_NOSCALE.cdt
+	gzip -dc $CDIR/$BAM\_$BED\_5read1-MIN128-MAX164_anti.cdt.gz | cut -f1,2,1003-1072 > $TEMP/$TARGET\_Distal_NOSCALE.cdt
 
-	# Calculate per-site Distal
-	gzip -dc $CDIR/$BAM\_$BED\_5read1-MIN128-MAX164_anti.cdt.gz \
-		| cut -f1,2,1003-1072 > $TEMP/$TARGET\_Distal_NOSCALE.cdt
+	# Scale by scaling factor
+	java -jar $SCRIPTMANAGER read-analysis scale-matrix -s $FACTOR $TEMP/$TARGET\_Proximal_NOSCALE.cdt -o $TEMP/$TARGET\_Proximal.cdt
 	java -jar $SCRIPTMANAGER read-analysis scale-matrix -s $FACTOR $TEMP/$TARGET\_Distal_NOSCALE.cdt -o $TEMP/$TARGET\_Distal.cdt
 done
 
@@ -70,10 +70,10 @@ sed '1d' $TEMP/H3K27ac-H3_Proximal.tab | awk 'BEGIN {OFS="\t"}{z = (log(($2+1)/(
 sed '1d' $TEMP/H3K27ac-H3_Distal.tab   | awk 'BEGIN {OFS="\t"}{z = (log(($2+1)/($3+1))/log(2)); print $1,z,"H3K27ac-H3_Distal"}'   > $TEMP/H3K27ac-H3_Distal.density
 
 # Compile density info
-cat $TEMP/*.density > F4/d/DensityInfo.tab
+cat $TEMP/*.density > S7/e/DensityInfo.tab
 
 # Generate violin plot
-python $VIOLIN -i <(cut -f2,3 F4/d/DensityInfo.tab) -o F4/d/DensityInfo.svg \
+python $VIOLIN -i <(cut -f2,3 S7/e/DensityInfo.tab) -o S7/e/DensityInfo.svg \
 	--width 8 --height 4 --preset1 \
 	--title "Density at +1 nucleosome" \
 	--xlabel "modification" --ylabel "Density (log2)"
