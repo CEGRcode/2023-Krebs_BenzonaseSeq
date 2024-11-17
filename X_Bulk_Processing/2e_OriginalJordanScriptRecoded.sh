71d76c21dcbdb44fadaecee880ed6f48c67ac69a
owlang <oliviawlang@gmail.com>
2024-11-17 12:11:33 -0500

    restructure X_Bulk.../2c-2e filepaths

--- X_Bulk_Processing/2e_OriginalJordanScriptRecoded.sh
+++ X_Bulk_Processing/2e_OriginalJordanScriptRecoded.sh
@@ -29,12 +29,25 @@ module load anaconda
 source activate /storage/group/bfp2/default/owl5022-OliviaLang/conda/bx
 
 # Load configs
-TF=`sed "${SLURM_ARRAY_TASK_ID}q;d" $METADATA | awk '{print $1}'`
-JASPAR=`sed "${SLURM_ARRAY_TASK_ID}q;d" $METADATA | awk '{print $1}'`
-ENCODE=`sed "${SLURM_ARRAY_TASK_ID}q;d" $METADATA | awk '{print $1}'`
+TARGET=`sed "${SLURM_ARRAY_TASK_ID}q;d" $METADATA | awk '{print $1}'`
+JASPAR=`sed "${SLURM_ARRAY_TASK_ID}q;d" $METADATA | awk '{print $2}'`
+ENCODE=`sed "${SLURM_ARRAY_TASK_ID}q;d" $METADATA | awk '{print $3}'`
 
 # Inputs and outputs
+MOTIF=../data/RefPT-JASPAR
 GENOME=../data/hg38_files/hg38.fa
+
+RUNID=${TARGET}_${JASPAR}
+BEDFILE=$MOTIF/1000bp/${RUNID}_SORT-TFnucRatio_GROUP-Quartile4_1000bp.bed
+BED=`basename $BEDFILE ".bed"`
+
+RUNID=${TARGET}_${JASPAR}
+ODIR=Library/DNAShape_${RUNID}
+[ -d $ODIR ] || mkdir -p $ODIR
+[ -d logs ] || mkdir logs
+
+BASE=$ODIR/$BED
+MASKED_region=${BASE}_masked.tab
 
 #set bedfiles
 QUARTILE4=/storage/group/bfp2/default/juk398-JordanKrebs/NucleosomeAtlas_project/240909_TFBS/CTCF_NucOccupancy_settings_pipeline_MA1929_1_240910/MA1929_1_final_1000bp_intersected_164bp_category4_1000bp.bed
@@ -52,50 +65,36 @@ MAX_MIN_SCALE=../bin/max_min_scale_v2_241006.py
 FORMAT=../bin/format_240922.py
 FINAL=../bin/final_240922.py
 
-#set output file names
-DNAshape=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1}')
-HelT=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_AVG_HelT.out"}')
-MGW=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_AVG_MGW.out"}')
-PropT=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_AVG_PropT.out"}')
-Roll=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_AVG_Roll.out"}')
-HelT_3=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_HelT_smoothed_3bp.tab"}')
-MGW_3=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_MGW_smoothed_3bp.tab"}')
-PropT_3=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_PropT_smoothed_3bp.tab"}')
-Roll_3=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_Roll_smoothed_3bp.tab"}')
-HelT_scale=$(echo "$HelT_3" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_scale.tab"}')
-MGW_scale=$(echo "$MGW_3" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_scale.tab"}')
-PropT_scale=$(echo "$PropT_3" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_scale.tab"}')
-Roll_scale=$(echo "$Roll_3" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_scale.tab"}')
-HelT_final=$(echo "01_""$HelT" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_final.tab"}')
-MGW_final=$(echo "02_""$MGW" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_final.tab"}')
-PropT_final=$(echo "03_""$PropT" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_final.tab"}')
-Roll_final=$(echo "04_""$Roll" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_final.tab"}')
-FILE_final=$(echo "$QUARTILE4" | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_final_file.tab"}')
-NT_count=$(echo $MEME | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_NT_count.tab"}')
-MASKED_region=$(echo $MEME | rev | cut -d"/" -f1 | rev | awk -F. '{print $1"_masked.tab"}')
+#extract number of NTs from MEME file
+python $EXTRACT $MEME ${BASE}_NT_count.tab
 
-sampleID=CTCF_DNAshape_Quartile4_v7_241006.slurm
+#determine the 5' and 3' boundaries of the motif masked region relative to the center column of tab files at column 256
+python $MASKED ${BASE}_NT_count.tab $MASKED_region
 
 #determine DNA shape with scriptmanager
-java -Djava.awt.headless=true -jar $SCRIPTMANAGER sequence-analysis dna-shape-bed --all --avg-composite -o=$DNAshape $GENOME $QUARTILE4
+java -Djava.awt.headless=true -jar $SCRIPTMANAGER sequence-analysis dna-shape-bed --all --composite -o $BASE $GENOME $BEDFILE
+
 #apply 3 bp smoothing
-python $SMOOTH3 $HelT $HelT_3
-python $SMOOTH3 $MGW $MGW_3
-python $SMOOTH3 $PropT $PropT_3
-python $SMOOTH3 $Roll $Roll_3
-#extract number of NTs from MEME file
-python $EXTRACT $MEME $NT_count
-#determine the 5' and 3' boundaries of the motif masked region relative to the center column of tab files at column 256
-python $MASKED $NT_count $MASKED_region
+python $SMOOTH3 ${BASE}_AVG_HelT.out ${BASE}_HelT_smooth3.tab
+python $SMOOTH3 ${BASE}_AVG_MGW.out ${BASE}_MGW_smooth3.tab
+python $SMOOTH3 ${BASE}_AVG_PropT.out ${BASE}_PropT_smooth3.tab
+python $SMOOTH3 ${BASE}_AVG_Roll.out ${BASE}_Roll_smooth3.tab
+
 #determine max scale for +/- 2bp arbitary units
-python $MAX_MIN_SCALE $HelT_3 $MASKED_region $HelT_scale
-python $MAX_MIN_SCALE $MGW_3 $MASKED_region $MGW_scale
-python $MAX_MIN_SCALE $PropT_3 $MASKED_region $PropT_scale
-python $MAX_MIN_SCALE $Roll_3 $MASKED_region $Roll_scale
+python $MAX_MIN_SCALE ${BASE}_HelT_smooth3.tab $MASKED_region ${BASE}_HelT_smooth3_scale.tab
+python $MAX_MIN_SCALE ${BASE}_MGW_smooth3.tab $MASKED_region ${BASE}_MGW_smooth3_scale.tab
+python $MAX_MIN_SCALE ${BASE}_PropT_smooth3.tab $MASKED_region ${BASE}_PropT_smooth3_scale.tab
+python $MAX_MIN_SCALE ${BASE}_Roll_smooth3.tab $MASKED_region ${BASE}_Roll_smooth3_scale.tab
+
 #change file name of OUT file and swap signs (if most of values are negative)
-python $FORMAT $HelT_scale $HelT $HelT_final
-python $FORMAT $MGW_scale $MGW $MGW_final
-python $FORMAT $PropT_scale $PropT $PropT_final
-python $FORMAT $Roll_scale $Roll $Roll_final
+python $FORMAT ${BASE}_HelT_smooth3_scale.tab ${BASE}_AVG_HelT.out $ODIR/01_${BED}_AVG_HelT_final.tab
+python $FORMAT ${BASE}_MGW_smooth3_scale.tab ${BASE}_AVG_MGW.out $ODIR/02_${BED}_AVG_MGW_final.tab
+python $FORMAT ${BASE}_PropT_smooth3_scale.tab ${BASE}_AVG_PropT.out $ODIR/03_${BED}_AVG_PropT_final.tab
+python $FORMAT ${BASE}_Roll_smooth3_scale.tab ${BASE}_AVG_Roll.out $ODIR/04_${BED}_AVG_Roll_final.tab
+
 #make file of scale and indication if values were swapped
-python $FINAL $HelT_scale $MGW_scale $PropT_scale $Roll_scale $FILE_final
+python $FINAL ${BASE}_HelT_smooth3_scale.tab \
+			${BASE}_MGW_smooth3_scale.tab \
+			${BASE}_PropT_smooth3_scale.tab \
+			${BASE}_Roll_smooth3_scale.tab \
+			${BASE}_final_file.tab
